34e359cdc7d6009419b6f9830582e8d625bdf641
Mikhail Sazanov <m@sazanof.ru>
2022-06-09 18:23:22 +0300

    Lost avatars in message list & thread envelope
    Signed-off-by: Mikhail Sazanov <m@sazanof.ru>

--- lib/Service/ContactsIntegration.php
+++ lib/Service/ContactsIntegration.php
@@ -106,12 +106,14 @@ class ContactsIntegration {
 	 * @return false|null|string
 	 */
 	public function getPhoto(string $email) {
-		$result = $this->contactsManager->search($email, ['EMAIL']);
-		if (count($result) > 0) {
-			if (isset($result[0]['PHOTO'])) {
-				return $this->getPhotoUri($result[0]['PHOTO']);
+		$result = $this->contactsManager->search($email, ['EMAIL'], ['escape_like_param' => false]);
+		foreach ($result as $contact){
+			if (!isset($contact['PHOTO']) || empty($contact['PHOTO'])) {
+				continue;
 			}
+			return $this->getPhotoUri($contact['PHOTO']);
 		}
+
 		return null;
 	}
 
